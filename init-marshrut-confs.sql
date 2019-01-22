3a84636c5a1db36571ecd848493687cdf6ec9613
Dimitryos <dimitry@extech.ru>
2019-01-22 15:45:48 +0300

    Изменения в структуре marshrut_confs

---
+++ init-marshrut-confs.sql
@@ -0,0 +1,42 @@
+
+set max_heap_table_size = 32000000;
+
+drop table if exists marshrut_confs;
+
+CREATE TABLE marshrut_confs (
+    id_marshrut SMALLINT UNSIGNED, 
+    id_vagon_category TINYINT UNSIGNED, 
+    id_vagon_type TINYINT UNSIGNED, 
+    vagon_ord_num TINYINT UNSIGNED, 
+    coupe_num TINYINT UNSIGNED, 
+    seat_num TINYINT UNSIGNED, 
+    is_upper TINYINT UNSIGNED, 
+    is_invalid TINYINT UNSIGNED, 
+    gender_constraints TINYINT UNSIGNED, 
+    id_service_class TINYINT UNSIGNED, 
+    price_basic DECIMAL(5,3) UNSIGNED, 
+    k DECIMAL(4,3)
+)
+ENGINE=MEMORY
+select distinct
+    id_marshrut, 
+    vcat.id_vagon_category, 
+    sc.id_vagon_type, 
+    vagon_ord_num, 
+    coupe_num, 
+    seat_num, 
+    is_upper, 
+    is_invalid, 
+    gender_constraints, 
+    id_service_class, 
+    price_basic, 
+    k
+from 
+    marshrut as m 
+    inner join sostav_conf as sc using(id_sostav_type)
+    inner join vagon_conf as vc on (sc.id_vagon_type=vc.id_vagon_type)
+    inner join vagon_type as vt on (vc.id_vagon_type=vt.id_vagon_type)
+    inner join vagon_category as vcat using(id_vagon_category)
+;
+
+CREATE INDEX idx_id_marshrut_mc ON marshrut_confs (id_marshrut, vagon_ord_num, seat_num) USING BTREE;
--- query.sql
+++ query.sql
@@ -1,20 +1,47 @@
+
 /*
-CREATE TABLE `trip_seats` (
-  `id_trip` int unsigned NOT NULL,
-  `id_station` smallint unsigned NOT NULL,
-  `vagon_ord_num` tinyint unsigned NOT NULL,
-  `seat_num` tinyint unsigned NOT NULL,
-  `coupe_num` tinyint unsigned DEFAULT NULL,
-  `is_reserved` tinyint unsigned NOT NULL DEFAULT '0',
-  `gender_constraints` tinyint unsigned DEFAULT NULL,
-  `gender_constraints_vc` tinyint unsigned DEFAULT NULL,
-  
-  INDEX `idx_trip_station` (`id_trip`,`id_station`)
-) 
-ENGINE=InnoDB 
-COMMENT='Расклад по местам для заданной поездки (партиционирование по хэшу id поездки)'
-PARTITION BY HASH(id_trip)
+drop temporary table if exists marshrut_confs;
+
+create temporary table marshrut_confs as
+select distinct
+    id_marshrut, 
+    vcat.id_vagon_category, 
+    sc.id_vagon_type, 
+    p.`name` as perevozchik_name, 
+    vagon_ord_num, coupe_num, 
+    seat_num, 
+    is_upper, 
+    is_invalid, 
+    gender_constraints, 
+    sc.id_service_class, 
+    service_class_code,
+    price_basic, 
+    k
+from 
+    marshrut as m 
+    inner join sostav_conf as sc using(id_sostav_type)
+    inner join vagon_conf as vc on (sc.id_vagon_type=vc.id_vagon_type)
+    inner join vagon_type as vt on (vc.id_vagon_type=vt.id_vagon_type)
+    inner join vagon_category as vcat using(id_vagon_category)
+    inner join perevozchik as p on vt.id_perevozchik=p.id_perevozchik
+    inner join service_class as srvc on sc.id_service_class=srvc.id_service_class
+order by
+    id_marshrut, vagon_ord_num, seat_num
 ;
 */
-
-desc trip_seats;
+select
+    trs.vagon_ord_num, 
+    if(is_upper>0, 'upper', 'lower') as placement,
+    min(round(price_basic*k*251)) as price_from, 
+    any_value(perevozchik_name) as perevozchik_name, 
+    any_value(service_class_code) as service_class_code, 
+    sum(if(id_vagon_type=29, 1, 0)) as has_heavy_luggage_coupe,
+    sum(is_invalid) as has_invalid_seats
+from 
+    trip_seats as trs
+    inner join trip as tr using(id_trip)
+    inner join marshrut_confs as mc on (tr.id_marshrut = mc.id_marshrut and trs.vagon_ord_num = mc.vagon_ord_num and trs.seat_num = mc.seat_num)
+where 
+    trs.id_trip=@id_trip and trs.id_station=@id_station_a and id_ticket_order is null and mc.id_vagon_category=5
+group by vagon_ord_num, is_upper
+;
